1551275ecd16a49967ffa6cfe7cb4fab5f2c09e2
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-05-23 19:09:59 +0300

    Make navbar sticky on small screen

--- src/cmps/Navbar.tsx
+++ src/cmps/Navbar.tsx
@@ -22,50 +22,48 @@ export function Navbar() {
   }
 
   return (
-    <nav>
-      <div className="navbar bg-base-100 small-container sm:big-container">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-            </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[100] p-2 shadow bg-base-100 rounded-box w-52">
-              <NavLinks />
-            </ul>
+    <nav className="navbar bg-base-100 sticky top-0 px-8 z-10 sm:static small-container sm:big-container">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
           </div>
-        </div>
-        <div className="navbar-center">
-          <Link to={'/jobs'} className=" text-2xl sm:text-3xl font-mono font-bold">JobTracker</Link>
-        </div>
-        <div className="navbar-end hidden sm:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <details>
-                <summary className='bg-sky-400 text-white text-lg hover:bg-sky-600'>
-                  <CgProfile className='text-2xl' />
-                  {user?.fullName}
-                </summary>
-                <ul className="p-2 bg-base-100 rounded-t-none">
-                  <li><button onClick={onLogout}>logout</button></li>
-                </ul>
-              </details>
-            </li>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[100] p-2 shadow bg-base-100 rounded-box w-52">
+            <NavLinks />
           </ul>
         </div>
-        <div className="navbar-end flex sm:hidden">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <details>
-                <summary className='bg-sky-400 text-white text-lg hover:bg-sky-600'>
-                  <CgProfile className='text-2xl' />
-                </summary>
-                <ul className="p-2 bg-base-100 rounded-t-none">
-                  <li><button onClick={onLogout}>logout</button></li>
-                </ul>
-              </details>
-            </li>
-          </ul>
-        </div>
+      </div>
+      <div className="navbar-center">
+        <Link to={'/jobs'} className=" text-2xl sm:text-3xl font-mono font-bold">JobTracker</Link>
+      </div>
+      <div className="navbar-end hidden sm:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <details>
+              <summary className='bg-sky-400 text-white text-lg hover:bg-sky-600'>
+                <CgProfile className='text-2xl' />
+                {user?.fullName}
+              </summary>
+              <ul className="p-2 bg-base-100 rounded-t-none">
+                <li><button onClick={onLogout}>logout</button></li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end flex sm:hidden">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <details>
+              <summary className='bg-sky-400 text-white text-lg hover:bg-sky-600'>
+                <CgProfile className='text-2xl' />
+              </summary>
+              <ul className="p-2 bg-base-100 rounded-t-none">
+                <li><button onClick={onLogout}>logout</button></li>
+              </ul>
+            </details>
+          </li>
+        </ul>
       </div>
     </nav>
   )
