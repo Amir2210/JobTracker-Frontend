02a72661e64e352a5381cee1b1c2af21d07a47a3
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-09-02 11:31:02 +0300

    Remove border from add button

--- src/cmps/AddJobButton.tsx
+++ src/cmps/AddJobButton.tsx
@@ -5,9 +5,9 @@ function AddJobButton() {
   return (
     <div className="card glass bg-white fixed sm:top-32 sm:left-32 sm:bottom-auto sm:right-auto bottom-5 right-5">
       <div className="card-body justify-center items-center p-5 sm:p-8">
-        <h2 className="card-title font-semibold">Add Job</h2>
-        <div className="card-actions border-none">
-          <Link to={'/addJob'} className="btn bg-sky-400 text-white"><FaPlus />
+        <h2 className="card-title font-semibold text-black">Add Job</h2>
+        <div className="card-actions ">
+          <Link to={'/addJob'} className="btn bg-sky-400 text-white border-none"><FaPlus />
           </Link>
         </div>
       </div>
