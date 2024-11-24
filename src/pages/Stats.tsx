cc43b4981b9abd104df4065ccf97f80c2062e3ae
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-11-24 15:57:55 +0200

    Change the stats page to work with theme mode

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -17,12 +17,12 @@ export function Stats() {
   return (
     <section>
       <Navbar />
-      <div className='bg-zinc-100 min-h-screen'>
+      <div className='bg-base-200 min-h-screen'>
         <div className='small-container sm:big-container sm:mt-4 sm:py-4 py-2 '>
           <div className='grid grid-cols-1 sm:grid-cols-2 gap-5'>
             <div className='sm:shadow-xl sm:mt-4 p-8 rounded-lg bg-white border-b-4 border-orange-400'>
               <div className='flex items-center justify-between'>
-                <p className='text-5xl'>{pendingJobs?.length}</p>
+                <p className='text-5xl text-orange-600'>{pendingJobs?.length}</p>
                 <div className='bg-orange-200 text-orange-600 p-2 rounded-md'>
                   <MdOutlinePendingActions className='text-5xl' />
                 </div>
@@ -31,7 +31,7 @@ export function Stats() {
             </div>
             <div className='sm:shadow-xl sm:mt-4 p-8 rounded-lg bg-white border-b-4 border-blue-600'>
               <div className='flex items-center justify-between'>
-                <p className='text-5xl'>{interviewJobs?.length}</p>
+                <p className='text-5xl text-blue-600'>{interviewJobs?.length}</p>
                 <div className='bg-blue-200 text-blue-600 p-2 rounded-md'>
                   <AiOutlineSchedule className='text-5xl' />
                 </div>
@@ -40,7 +40,7 @@ export function Stats() {
             </div>
             <div className='sm:shadow-xl sm:mt-4 p-8 rounded-lg bg-white border-b-4 border-red-600'>
               <div className='flex items-center justify-between'>
-                <p className='text-5xl'>{declinedJobs?.length}</p>
+                <p className='text-5xl text-red-600'>{declinedJobs?.length}</p>
                 <div className='bg-red-200 text-red-600 p-2 rounded-md'>
                   <FaBug className='text-5xl' />
                 </div>
@@ -51,7 +51,7 @@ export function Stats() {
 
             <div className='sm:shadow-xl sm:mt-4 p-8 rounded-lg bg-white border-b-4 border-purple-600'>
               <div className='flex items-center justify-between'>
-                <p className='text-5xl'>{hrInterviewJobs?.length}</p>
+                <p className='text-5xl text-purple-600'>{hrInterviewJobs?.length}</p>
                 <div className='bg-purple-200 text-purple-600 p-2 rounded-md'>
                   <ImProfile className='text-5xl' />
                 </div>
@@ -59,7 +59,7 @@ export function Stats() {
               <p className='mt-4 font-mono text-2xl text-sky-950'>HR Interviews</p>
             </div>
           </div>
-          <div className='flex justify-center my-8 sm:h-72'>
+          <div className='flex justify-center my-8 sm:h-72 chart-txt-color'>
             <DoughnutChart
               pendingJobs={pendingJobs?.length || 0}
               interviewJobs={interviewJobs?.length || 0}
