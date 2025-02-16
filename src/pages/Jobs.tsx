3b431e3e041f47f1a437c6b2be26e535a8119e2c
Amir Yankolovich <amiryankolovich2000@gmail.com>
2025-02-16 13:49:47 +0200

    Delete comments

--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -67,67 +67,7 @@ export function Jobs() {
         <div className='small-container sm:big-container sm:mt-4 sm:py-4 py-2  '>
           <FilterJob filterBy={filterBy} onSetFilter={onSetFilter} sortBy={sortBy} onSetSort={onSetSort} />
           <h1 className='text-2xl capitalize font-medium'>{totalJobs} {totalJobs === 1 ? 'job' : 'jobs'} found</h1>
-          /////////
           <JobsList isLoading={isLoading} userJobs={userJobs} lastJobRef={lastJobRef} />
-          {/* <div className='grid sm:grid-cols-2 gap-5 mt-4'>
-            {!isLoading && userJobs?.map((job, index: number) => <article ref={index === userJobs.length - 1 ? lastJobRef : null} key={job._id} className='sm:mt-4 sm:py-4 py-2 px-2 rounded-lg bg-base-100 h-fit'>
-              <div className='flex gap-8 border-solid border-indigo-100 border-b py-3 px-3'>
-                <div className={`text-4xl text-white font-mono font-bold size-14 flex justify-center items-center rounded-lg ${statusImgBgColor(job.status)}`}>{statusImg(job.status)}</div>
-                <div>
-                  <h1 className=' text-xl capitalize mb-1'>{job.position}</h1>
-                  <h2 className=' capitalize'>{job.company}</h2>
-                </div>
-                <div className='flex ml-auto items-center'>
-                  <CiStar className='text-4xl' />
-                </div>
-              </div>
-              <div className='grid sm:grid-cols-2 gap-6 sm:gap-8 py-3 px-3'>
-                <div className='flex items-center gap-5'>
-                  <FaLocationArrow className='text-slate-400' />
-                  <p className=' text-lg capitalize'>{job.jobLocation}</p>
-                </div>
-                <div className='flex items-center gap-5'>
-                  <FaSuitcase className='text-slate-400' />
-                  <p className=' text-lg capitalize'>{job.jobType}</p>
-                </div>
-              </div>
-              <div className='grid sm:grid-cols-2 gap-6 sm:gap-8 py-3 px-3'>
-                <div className='flex items-center gap-5'>
-                  <FaCalendarAlt className='text-slate-400' />
-                  <p className=' text-lg capitalize'>{formatDate(job.time)}</p>
-                </div>
-                <div tabIndex={0} className=" sm:hidden collapse collapse-arrow border-indigo-100 border">
-                  <div className="collapse-title text-xl">Description:</div>
-                  <div className="collapse-content">
-                    <p>{job.description}</p>
-                  </div>
-                </div>
-                <div className='flex items-center gap-5'>
-                  <p className={`${statusClass(job.status)} btn  font-medium rounded-md cursor-auto`}>{job.status}</p>
-                </div>
-              </div>
-              <div tabIndex={0} className="hidden sm:grid collapse collapse-arrow border-indigo-100 border">
-                <div className="collapse-title text-xl">Description:</div>
-                <div className="collapse-content">
-                  <p>{job.description}</p>
-                </div>
-              </div>
-              <div className='flex gap-5 py-3 px-3'>
-                <div className='flex items-center '>
-                  <Link to={'/addJob'} state={{ job }} className='btn capitalize bg-lime-100 text-lime-600 rounded-md hover:bg-lime-200 border-none'>edit</Link>
-                </div>
-                <div className='flex items-center '>
-                  <button onClick={() => onDeleteJob(job._id)} className='btn capitalize bg-red-100 text-red-600 shadow-lg shadow-red100/50 hover:bg-red-200 border-none'>delete</button>
-                </div>
-              </div>
-            </article>)}
-            {isLoading && <FallingLines
-              color="#38bdf8"
-              width="100"
-              visible={true}
-            />}
-          </div> */}
-          //////////
           <Pagination totalJobs={totalJobs} filterBy={filterBy} onSetFilter={onSetFilter} />
           {!userJobs?.length && !filterBy.txt && !filterBy.status && !filterBy.jobType && <div className='flex flex-col justify-center items-center mt-4'>
             <img className='size-48 sm:size-96' src="https://res.cloudinary.com/dxm0sqcfp/image/upload/v1715154175/job%20tracker/ocfxopyi3lshmxzmucwd.svg" alt="" />
