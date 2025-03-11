0ed006e2af746b395367a5d6e5a77c724870da95
Amir Yankolovich <amiryankolovich2000@gmail.com>
2025-03-11 13:58:53 +0200

    Add meta data to the jobs page

--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux'
 import { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Helmet } from "react-helmet-async"
 //components
 import { Pagination } from '../cmps/Pagination'
 import AddJobButton from '../cmps/AddJobButton'
@@ -65,24 +66,30 @@ export function Jobs() {
   }
 
   return (
-    <section >
-      <Navbar />
-      <div className='bg-base-200 min-h-screen'>
-        <div className='small-container sm:big-container sm:mt-4 sm:py-4 py-2  '>
-          <FilterJob filterBy={filterBy} onSetFilter={onSetFilter} sortBy={sortBy} onSetSort={onSetSort} />
-          <div className='flex justify-between items-center'>
-            {isFavoriteShow ? <h1 className='text-2xl capitalize font-medium'>{userFavoriteJobs?.length} favorite {userFavoriteJobs?.length === 1 ? 'job' : 'jobs'} found</h1> : <h1 className='text-2xl capitalize font-medium'>{totalJobs} {totalJobs === 1 ? 'job' : 'jobs'} found</h1>}
-            <button onClick={() => setIsFavoriteShow(!isFavoriteShow)} className=' btn bg-sky-400 hover:bg-sky-600 text-white text-lg sm:text-xl capitalize font-medium'>{isFavoriteShow ? 'show all jobs' : 'show favorite jobs'}</button>
+    <>
+      <Helmet>
+        <title>Job Listings - JobTracker</title>
+        <meta name="description" content="Browse and filter your job applications on JobTracker. Find favorite jobs, sort by criteria, and manage your job search effectively." />
+      </Helmet>
+      <section >
+        <Navbar />
+        <div className='bg-base-200 min-h-screen'>
+          <div className='small-container sm:big-container sm:mt-4 sm:py-4 py-2  '>
+            <FilterJob filterBy={filterBy} onSetFilter={onSetFilter} sortBy={sortBy} onSetSort={onSetSort} />
+            <div className='flex justify-between items-center'>
+              {isFavoriteShow ? <h1 className='text-2xl capitalize font-medium'>{userFavoriteJobs?.length} favorite {userFavoriteJobs?.length === 1 ? 'job' : 'jobs'} found</h1> : <h1 className='text-2xl capitalize font-medium'>{totalJobs} {totalJobs === 1 ? 'job' : 'jobs'} found</h1>}
+              <button onClick={() => setIsFavoriteShow(!isFavoriteShow)} className=' btn bg-sky-400 hover:bg-sky-600 text-white text-lg sm:text-xl capitalize font-medium'>{isFavoriteShow ? 'show all jobs' : 'show favorite jobs'}</button>
+            </div>
+            <JobsList isDemoUser={isDemoUser} isLoading={isLoading} userJobs={userJobs} lastJobRef={lastJobRef} userFavoriteJobs={userFavoriteJobs} isFavoriteShow={isFavoriteShow} />
+            <Pagination isFavoriteShow={isFavoriteShow} totalFavoriteJobs={totalFavoriteJobs} totalJobs={totalJobs} filterBy={filterBy} onSetFilter={onSetFilter} />
+            {!userJobs?.length && !filterBy.txt && !filterBy.status && !filterBy.jobType && <div className='flex flex-col justify-center items-center mt-4'>
+              <img className='size-48 sm:size-96' src="https://res.cloudinary.com/dxm0sqcfp/image/upload/v1715154175/job%20tracker/ocfxopyi3lshmxzmucwd.svg" alt="" />
+              <p className='capitalize mt-4 text-2xl sm:text-4xl'>add your first job <span className='link text-sky-400'><Link to={'/addJob'}>here</Link> </span></p>
+            </div>}
           </div>
-          <JobsList isDemoUser={isDemoUser} isLoading={isLoading} userJobs={userJobs} lastJobRef={lastJobRef} userFavoriteJobs={userFavoriteJobs} isFavoriteShow={isFavoriteShow} />
-          <Pagination isFavoriteShow={isFavoriteShow} totalFavoriteJobs={totalFavoriteJobs} totalJobs={totalJobs} filterBy={filterBy} onSetFilter={onSetFilter} />
-          {!userJobs?.length && !filterBy.txt && !filterBy.status && !filterBy.jobType && <div className='flex flex-col justify-center items-center mt-4'>
-            <img className='size-48 sm:size-96' src="https://res.cloudinary.com/dxm0sqcfp/image/upload/v1715154175/job%20tracker/ocfxopyi3lshmxzmucwd.svg" alt="" />
-            <p className='capitalize mt-4 text-2xl sm:text-4xl'>add your first job <span className='link text-sky-400'><Link to={'/addJob'}>here</Link> </span></p>
-          </div>}
+          {!isLastJobVisible && <AddJobButton />}
         </div>
-        {!isLastJobVisible && <AddJobButton />}
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
