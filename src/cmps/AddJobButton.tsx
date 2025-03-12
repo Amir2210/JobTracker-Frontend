a4a43d839876fe44771800c73cdb11e3d94bd311
Amir Yankolovich <amiryankolovich2000@gmail.com>
2025-03-12 09:51:07 +0200

    Add more aria label

--- src/cmps/AddJobButton.tsx
+++ src/cmps/AddJobButton.tsx
@@ -7,7 +7,7 @@ function AddJobButton() {
       <div className="card-body justify-center items-center p-5 sm:p-8">
         <h2 className="card-title  text-black">Add Job</h2>
         <div className="card-actions ">
-          <Link to={'/addJob'} className="btn bg-sky-400 text-white border-indigo-100 border"><FaPlus />
+          <Link to={'/addJob'} className="btn bg-sky-400 text-white border-indigo-100 border" aria-label='Add new Job button'><FaPlus />
           </Link>
         </div>
       </div>
--- src/cmps/Navbar.tsx
+++ src/cmps/Navbar.tsx
@@ -55,7 +55,7 @@ export function Navbar() {
     <nav className="navbar bg-base-100 sticky top-0 px-8 z-10 sm:static small-container sm:big-container">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle" aria-label='Toggle menu'>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
           </div>
           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[100] p-2 shadow bg-base-100 rounded-box w-52">
